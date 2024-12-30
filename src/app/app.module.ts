fb47143628b3156af81c03f9bb9e5b1b6667cf71
kaviya1107 <kavithakaviya0326@gmail.com>
2024-12-30 17:41:28 +0530

    add

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http'; // Import this module
 import { AppComponent } from './app.component';
+import { NgModel } from '@angular/forms';
 
 @NgModule({
   declarations: [
